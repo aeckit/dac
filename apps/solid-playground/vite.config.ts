448822820bb625f7cbcef79644f6ad9f220278dc
Nguyen Ngo <mnguyenngo@gmail.com>
2026-09-24 09:32:33 -0700

    fix(solid-playground): change vite base url to /dac/ for gh-pages deployment

--- apps/solid-playground/vite.config.ts
+++ apps/solid-playground/vite.config.ts
@@ -3,7 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  base: '/dac/playground/',
+  base: '/dac/',
   server: {
     port: 5174,
     hmr: { port: 5174 }
